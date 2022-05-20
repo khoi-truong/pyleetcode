037a32ef6f707a8d4f9d84f136f25f6af1ce14aa
Khoi Truong <khoi.truongminh@gmail.com>
2022-05-20 17:48:20 +0700

    feat: reverse level order traverse

--- breadth_first_search/reverse_level_order_traverse.py
+++ breadth_first_search/reverse_level_order_traverse.py
@@ -1,17 +1,18 @@
 """
-Problem: https://leetcode.com/problems/binary-tree-level-order-traversal/
+Problem: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 
 Given a binary tree, populate an array to represent its level-by-level \
-    traversal. You should populate the values of all nodes of each level \
-    from left to right in separate sub-arrays.
+traversal in reverse order, i.e., the lowest level comes first. You should \
+populate the values of all nodes in each level from left to right in separate \
+sub-arrays.
 """
 from collections import deque
 
 from .tree_node import TreeNode
 
 
-def traverse(root: TreeNode):
-    result = []
+def reverse_level_order_traverse(root: TreeNode):
+    result = deque()
     if root is None:
         return result
 
@@ -27,5 +28,5 @@ def traverse(root: TreeNode):
                 queue.append(current_node.left)
             if current_node.right:
                 queue.append(current_node.right)
-        result.append(current_level)
-    return result
+        result.appendleft(current_level)
+    return list(result)
--- breadth_first_search/tests/test_traverse.py
+++ breadth_first_search/tests/test_traverse.py
@@ -2,18 +2,19 @@
 Unit tests for BFS problems
 """
 
-from ..traverse import traverse
+from ..level_order_traverse import level_order_traverse
+from ..reverse_level_order_traverse import reverse_level_order_traverse
 from ..tree_node import TreeNode
 
 
-def test_traverse():
+def test_level_order_traverse():
     root = TreeNode(12)
     root.left = TreeNode(7)
     root.right = TreeNode(1)
     root.left.left = TreeNode(9)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    assert traverse(root) == [[12], [7, 1], [9, 10, 5]]
+    assert level_order_traverse(root) == [[12], [7, 1], [9, 10, 5]]
 
     root = TreeNode(1)
     root.left = TreeNode(2)
@@ -22,4 +23,23 @@ def test_traverse():
     root.left.right = TreeNode(5)
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(7)
-    assert traverse(root) == [[1], [2, 3], [4, 5, 6, 7]]
+    assert level_order_traverse(root) == [[1], [2, 3], [4, 5, 6, 7]]
+
+
+def test_reverse_level_order_traverse():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    assert reverse_level_order_traverse(root) == [[9, 10, 5], [7, 1], [12]]
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    assert reverse_level_order_traverse(root) == [[4, 5, 6, 7], [2, 3], [1]]
