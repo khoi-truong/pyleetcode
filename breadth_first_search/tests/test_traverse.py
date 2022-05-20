ba53e66cfd5425828f49aef95bbef3978cde1618
Khoi Truong <khoi.truongminh@gmail.com>
2022-05-20 17:29:18 +0700

    test: add test case for BFS traversal

--- breadth_first_search/tests/test_traverse.py
+++ breadth_first_search/tests/test_traverse.py
@@ -14,3 +14,12 @@ def test_traverse():
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
     assert traverse(root) == [[12], [7, 1], [9, 10, 5]]
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    assert traverse(root) == [[1], [2, 3], [4, 5, 6, 7]]
